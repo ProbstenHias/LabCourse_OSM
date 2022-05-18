00299983c1e31917b11c4fdfc7b8cdbf5cb40069
ProbstenHias <matthias.weilinger@gmx.de>
2022-05-18 10:18:39 +0200

    fixed haversine distance calculation

--- src/shortestPath/distance.go
+++ src/shortestPath/distance.go
@@ -2,11 +2,17 @@ package shortestPath
 
 import "math"
 
-const earthDiameter = 12_742_000
+const R = 6_371_000
 
-func haversine(p1 []float64, p2 []float64) uint32 {
-	const p = math.Pi / 180
-	var a = 0.5 - math.Cos((p2[0]-p1[0])*p)/2 + math.Cos(p1[0]*p)*math.Cos(p2[0])*(1-math.Cos((p2[1]-p1[1])*p))/2
-	var d = earthDiameter * math.Asin(math.Sqrt(a))
-	return uint32(d)
+func haversine(p1 []float64, p2 []float64) int {
+	var dLat = deg2grad(p2[0] - p1[0])
+	var dLon = deg2grad(p2[1] - p1[1])
+	var a = math.Sin(dLat/2)*math.Sin(dLat/2) + math.Cos(deg2grad(p1[0]))*math.Cos(deg2grad(p2[0]))*math.Sin(dLon/2)*math.Sin(dLon/2)
+	var c = 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	var d = R * c
+	return int(d)
+}
+
+func deg2grad(deg float64) float64 {
+	return deg * (math.Pi / 180)
 }
