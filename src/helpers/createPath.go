b2e3a3859d3f8c8abc8bcd06f25654ad5426a9c2
ProbstenHias <matthias.weilinger@gmx.de>
2022-05-27 16:26:21 +0200

    creates a path from end to start from a given prev arr as is generated by a dijkstra run

---
+++ src/helpers/createPath.go
@@ -0,0 +1,15 @@
+package helpers
+
+import "OSM/src/datastructures"
+
+func CreatePathFromPrev(start, end int32, prev []int32, graph datastructures.Graph) [][]float64 {
+	var path [][]float64
+	currNode := end
+	for {
+		path = append(path, graph.Nodes[currNode])
+		if currNode == start {
+			return path
+		}
+		currNode = prev[currNode]
+	}
+}
