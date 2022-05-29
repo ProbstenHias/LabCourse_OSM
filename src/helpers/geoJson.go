bb0fc745f0ab9671affc0860893e512d0184d040
ProbstenHias <matthias.weilinger@gmx.de>
2022-05-29 19:25:09 +0200

    added conversion from graph to geojson

--- src/helpers/geoJson.go
+++ src/helpers/geoJson.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"OSM/src/datastructures"
 	geojson "github.com/paulmach/go.geojson"
 	"log"
 	"os"
@@ -35,6 +36,28 @@ func NodesToPoints(nodes [][]float64) []byte {
 		feature.SetProperty("", nil)
 		fc.AddFeature(feature)
 	}
+	rawJson, _ := fc.MarshalJSON()
+	return rawJson
+}
+
+func GraphToGeoJson(graph datastructures.Graph) []byte {
+	fc := geojson.NewFeatureCollection()
+	for i, node := range graph.Nodes {
+		pointFeature := geojson.NewPointFeature([]float64{node[1], node[0]})
+		pointFeature.SetProperty("idx", i)
+		fc.AddFeature(pointFeature)
+		edges := graph.GetAllOutgoingEdgesOfNode(int32(i))
+		for _, edgeIdx := range edges {
+			toNode := graph.Nodes[graph.Edges[edgeIdx]]
+			lineStringFeature := geojson.NewLineStringFeature([][]float64{
+				{node[1], node[0]},
+				{toNode[1], toNode[0]},
+			})
+			lineStringFeature.SetProperty("distance", graph.Distance[edgeIdx])
+			fc.AddFeature(lineStringFeature)
+		}
+	}
+	log.Printf("Created %d features\n", len(fc.Features))
 	rawJson, _ := fc.MarshalJSON()
 	return rawJson
 }
